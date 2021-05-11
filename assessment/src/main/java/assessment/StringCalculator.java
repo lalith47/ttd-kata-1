f50ce9247371a5491d99e2e66dade4eed43d27a1
lalith niddapu <lalithsundar.niddapu@gmail.com>
2021-05-11 19:39:36 +0530

    3

--- assessment/src/main/java/assessment/StringCalculator.java
+++ assessment/src/main/java/assessment/StringCalculator.java
@@ -3,6 +3,9 @@ package assessment;
 public class StringCalculator {
 
     public int add(String str) {
+        if(str.isEmpty()){
+            return 0;
+        }
         return Integer.valueOf(str);
     }
 
